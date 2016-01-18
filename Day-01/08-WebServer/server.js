10d9db0d30e3c0d13e35f9fa22370364d84a6e42
tkmagesh <tkmagesh77@gmail.com>
2016-01-18 15:51:50 +0530

    Hanling requests

--- Day-01/08-WebServer/server.js
+++ Day-01/08-WebServer/server.js
@@ -1,8 +1,27 @@
-var http = require('http');
+var http = require('http'),
+    fs = require('fs'),
+    path = require('path'),
+    url = require('url');
+
 var server = http.createServer(function(req, res){
-    res.write("<h1>Welcome to node.js</h1>");
-    res.write("You have requested the resource - " + req.url);
-    res.end();
+    console.log(__dirname);
+    var urlObj = url.parse(req.url);
+    var resource = path.join(__dirname, urlObj.pathname);
+    console.log(urlObj.pathname);
+    console.log(resource);
+    if (!fs.existsSync(resource)){
+        res.statusCode = 404;
+        res.end();
+        return;
+    }
+    var stream = fs.createReadStream(resource);
+    stream.on('data', function(dataChunk){
+        res.write(dataChunk);
+    });
+    stream.on('end', function(){
+        res.end();
+    });
+
 });
 server.listen(8080);
 console.log("Server listening on port 8080");
