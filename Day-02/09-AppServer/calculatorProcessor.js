5fbaa2663e97682dfdc3a5d41eda0c4c8e24fe71
tkmagesh <tkmagesh77@gmail.com>
2016-01-19 12:05:31 +0530

    Using Handlebars for generating views

--- Day-02/09-AppServer/calculatorProcessor.js
+++ Day-02/09-AppServer/calculatorProcessor.js
@@ -1,5 +1,8 @@
 var calculator = require('./calculator');
+var fs = require('fs');
+var handlebars = require('handlebars');
 var qs = require('querystring');
+var path = require('path');
 
 module.exports = function(req, res, next){
     if (req.url.pathname === '/calculator'){
@@ -7,8 +10,19 @@ module.exports = function(req, res, next){
             n1 = parseInt(req.field('n1')),
             n2 = parseInt(req.field('n2'));
         var result = calculator[operation](n1,n2);
-        res.write('result = ' + result);
-        res.end();
+        var viewModel = {
+            n1 : n1,
+            n2 : n2,
+            op : operation,
+            result : result
+        };
+        var templateFile = path.join(__dirname, './templates/calculatorResponse.hbs');
+        fs.readFile(templateFile, {encoding : 'utf8'}, function(err, fileContents){
+            var templateFn = handlebars.compile(fileContents);
+            var responseHTML = templateFn(viewModel);
+            res.write(responseHTML);
+            res.end();
+        });
         
     } else {
         next();
